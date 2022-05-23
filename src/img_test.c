bb8d213258e7edd1c538805ba9fa909ec5c798ad
ilyasher <ilyasherstyuk@gmail.com>
2022-05-22 20:37:06 -0700

    Add mars

--- src/img_test.c
+++ src/img_test.c
@@ -11,13 +11,12 @@
 
 #define RGBA(r, g, b, a) ((r) | ((g) << 8) | ((b) << 16) | ((a) << 24))
 #define RGB(r, g, b) RGBA(r, g, b, 0xff)
+#define ALPHA(c, a) ((c) | ((a) << 8))
 
 #define CAPAT(c, min, max) ((c) < (min) ? (min) : ((c) > (max) ? (max) : (c)))
 
-#define ALPHA(c, a) ((c) | ((a) << 8))
-
-#define W 512
-#define H 512
+#define W 256
+#define H 256
 
 typedef struct vec3_t {
     float x;
@@ -25,21 +24,37 @@ typedef struct vec3_t {
     float z;
 } vec3_t;
 
+float earth_height_function(float n);
+float mars_height_function(float n);
+int mars_color_function(float x, float y, float z, float t, int is_png);
+int earth_color_function(float x, float y, float z, float t, int is_png);
+
 float r = 0.45;
 float noise_amp = 0.1;
-float water_level = -0.005;
+float water_level = -1;
+// float water_level = -0.005;
 float snow_level = 0.025;
+
+float rotation_freq = -0.7;
+
+float (*height_function)(float) = mars_height_function;
+int   (*color_function)(float, float, float, float, int) = mars_color_function;
 
 // Simplex noise added up at a few octaves
 float noise(float x, float y, float z, float t) {
+
+    float x_rot = x*cos(rotation_freq * t) - z*sin(rotation_freq * t);
+    float z_rot = x*sin(rotation_freq * t) + z*cos(rotation_freq * t);
+    float y_rot = y;
+
     float power = 1;
     float n = 0;
     for (int freq = 2; freq < 256; freq *= 2) {
-        n += power * noise4(x*freq, y*freq, z*freq, t);
-        power /= 2;
+        n += power * noise4(x_rot*freq, y_rot*freq, z_rot*freq, 0);
+        power /= 2.2;
     }
-    n *= sqrt(fabs(n)); // makes the shape a little bit spikier
-    // n *= 2 * fabs(n); // makes the shape a little bit spikier
+
+    n = height_function(n);
     return n;
 }
 
@@ -47,7 +62,6 @@ float surface_height(float x, float y, float z, float t) {
     float norm = sqrt(x*x+y*y+z*z);
     float height = noise_amp*noise(r*x/norm, r*y/norm, r*z/norm, t);
     return height;
-    // return height > water_level ? height : water_level;
 }
 
 float sdf(float x, float y, float z, float t) {
@@ -78,7 +92,7 @@ void grad_sdf(vec3_t *grad, float x, float y, float z, float t) {
 // Given x, y, find which z is at the planet's surface
 // (try find z such that f(x, y, z, t) = 0)
 float ray_march_z(float x, float y, float z, float t) {
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 32; i++) {
         z = z - sdf(x, y, z, t);
     }
     return z;
@@ -95,13 +109,9 @@ void make_zs(float zs[H][W], uint8_t zs_valid[H][W], float t) {
             if (rrxxyy > 0) {
                 z = sqrt(rrxxyy);
             }
-            // z = find_z(x, y, z, t);
             z = ray_march_z(x, y, z, t);
             zs[i][j] = z;
-            // zs_valid[i][j] = (fabs(f(x, y, z, t)) < 1e-2);
             zs_valid[i][j] = (fabs(sdf(x, y, z, t)) < 1e-2);
-
-            // zs_valid[i][j] = 1;
         }
     }
 }
@@ -111,109 +121,118 @@ vec3_t lp = {-7, 2, 7};
 // Return number 0-1 representing how much lighting the point has.
 float lighting(float x, float y, float z, float t) {
 
-    vec3_t ray = lp;
-    vec3_t ld = {x-lp.x, y-lp.y, z-lp.z};
+    float ambient = 0.1;
+
+    vec3_t ray = {lp.x * cos(t), lp.y, lp.z -14*sin(t)};
+    vec3_t ld = {x-ray.x, y-ray.y, z-ray.z};
     normalize(&ld);
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 32; i++) {
         float dist = sdf(ray.x, ray.y, ray.z, t);
         ray.x += dist*ld.x;
         ray.y += dist*ld.y;
         ray.z += dist*ld.z;
     }
     float dist = sqrt((ray.x-x)*(ray.x-x)+(ray.y-y)*(ray.y-y)+(ray.z-z)*(ray.z-z));
-    // printf("%f, %f, %f\n", dist, sdf(ray.x, ray.y, ray.z, t), sdf(x, y, z, t));
     if (dist > 1e-2) {
         // In the shadow
-        return 0.1;
+        return ambient;
     }
 
     vec3_t grad;
     grad_sdf(&grad, x, y, z, t);
 
-    float l = -ld.x*grad.x - ld.y*grad.y - ld.z*grad.z;
-    l = l > 0 ? l : 0;
+    float diffuse = -ld.x*grad.x - ld.y*grad.y - ld.z*grad.z;
+    diffuse = diffuse > 0 ? diffuse : 0;
 
-    return 0.1 + 0.9 * l;
+    // float specular = grad.z > 0 ? grad.z : 0;
+    // specular = pow(specular, 256);
+
+    return ambient + 0.9 * diffuse;
 }
 
-void fill_texture_png(libattopng_t *png, float zs[H][W], uint8_t zs_valid[H][W], float t) {
+void fill_texture(void *pixels, float zs[H][W], uint8_t zs_valid[H][W], float t, int is_png) {
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
             if (!zs_valid[i][j]) {
-                libattopng_set_pixel(png, j, i, RGB(0, 0, 0));
+                if (is_png) {
+                    libattopng_set_pixel((libattopng_t *) pixels, j, i, RGB(0, 0, 0));
+                } else {
+                    ((uint8_t *)pixels)[i*H+j] = BLACK;
+                }
                 continue;
             }
-
             float x = (j - W/2)/(float)H;
             float y = (i - H/2)/(float)H;
             float z = zs[i][j];
-
-            float n = surface_height(x, y, z, t);
-
-            float light = lighting(x, y, z, t);
-            // float light = 1;
-            int c1 = CAPAT((100+n*2000) * light, 0, 255);
-
-            if (n < water_level) {
-                libattopng_set_pixel(png, j, i, RGB(c1, c1, (int) (255*light)));
-                continue;
-            }
-
-            if (n < snow_level) {
-                int c =  CAPAT((light * (100 + (int)(n*3000))), 0, 255);
-                // Add other colors too here if you wish!
-                libattopng_set_pixel(png, j, i, RGB(0, c, 0));
-
+            int color = color_function(x, y, z, t, is_png);
+            if (is_png) {
+                libattopng_set_pixel((libattopng_t *) pixels, j, i, color);
             } else {
-                int c =  CAPAT((int) (light * (100 + (int)(n*3000))), 0, 255);
-                // Add other colors too here if you wish!
-                libattopng_set_pixel(png, j, i, RGB(c, c, c));
+                ((uint8_t *)pixels)[i*H+j] = color;
             }
         }
     }
 }
 
-void fill_texture_gif(uint8_t *pixels, float zs[H][W], uint8_t zs_valid[H][W], float t) {
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            if (!zs_valid[i][j]) {
-                pixels[i*H+j] = BLACK;
-                continue;
-            }
-            float x = (j - W/2)/(float)H;
-            float y = (i - H/2)/(float)H;
-            float z = zs[i][j];
+float earth_height_function(float n) {
+    return n * sqrt(fabs(n)); // makes the shape a little bit spikier
+}
 
-            float n = surface_height(x, y, z, t);
-            // uint8_t c1 =  100+n*200;
+float mars_height_function(float n) {
+    n += 0.3;
+    n = -1./(500*n*n+4);// + 1/(1000*(x-0.3)*(x-0.3)+5);
+    return n * 0.5;
+}
 
-            float light = lighting(x, y, z, t);
-
-            if (n < water_level) {
-                int c = 48 + (int)(n*64);
-                c = c > 64 ? 64 : (c < 0 ? 0 : c);
-                pixels[i*H+j] = BLUE_TABLE[(int)(c*light)];
-                continue;
-            }
-
-            // Add other colors too here if you wish!
-            if (n < snow_level) {
-                int c = 16+(int)(n*96);
-                c = c > 64 ? 64 : (c < 0 ? 0 : c);
-                pixels[i*H+j] = GREEN_TABLE[(int)(c*light)];
-
-            } else {
-                // int c = 24+(int)(n*96);
-                int c = 63;
-                pixels[i*H+j] = GREY_TABLE[(int)(c*light)];
-            }
+int mars_color_function(float x, float y, float z, float t, int is_png) {
+    float light = lighting(x, y, z, t);
+    float n = surface_height(x, y, z, t);
+    if (is_png) {
+        float intensity = (800+n*10000) * light;
+        if (intensity < 500) {
+            int c = CAPAT(intensity / 500. * 255., 0, 255);
+            return RGB(c, 0, 0);
+        } else {
+            int c = CAPAT((intensity-500) / 500. * 255., 0, 255);
+            return RGB(255, c, c);
         }
+    }
+    // else is gif
+    return RED_TABLE[CAPAT((int)(light * (50 + 64*20*n)), 0, 63)];
+}
+
+int earth_color_function(float x, float y, float z, float t, int is_png) {
+    float light = lighting(x, y, z, t);
+    float n = surface_height(x, y, z, t);
+
+    if (n < water_level) {
+        if (is_png) {
+            int c = CAPAT((48+n*64) * light, 0, 255);
+            return RGB(c, c,  (int) (255*light));
+        }
+        int c = 48 + (int)(n*64);
+        return BLUE_TABLE[CAPAT((int)(c*light), 0, 63)];
+    }
+    if (n < snow_level) {
+        if (is_png) {
+            int c = CAPAT((light * (100 + (int)(n*3000))), 0, 255);
+            return RGB(0, c, 0);
+        }
+        int c = 16+(int)(n*96);
+        return GREEN_TABLE[CAPAT((int)(c*light), 0, 63)];
+    } else {
+        if (is_png) {
+            int c = CAPAT((light * (100 + (int)(n*3000))), 0, 255);
+            return RGB(c, c, c);
+        }
+        int c = 63;
+        return GREY_TABLE[(int)(c*light)];
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int do_gif = 0;
+    int do_gif = 1;
 
     // Array of z positions for every x, y
     float zs[H][W];
@@ -229,28 +248,29 @@ int main(int argc, char *argv[])
 
         float t = 0; // arbitrary
         make_zs(zs, zs_valid, t);
-        fill_texture_png(png, zs, zs_valid, t);
-        libattopng_save(png, "test_rgb_heights_shadows.png");
+        fill_texture(png, zs, zs_valid, t, 1);
+        libattopng_save(png, "mars_test.png");
         libattopng_destroy(png);
         return 0;
     }
     else {
 
         ge_GIF *gif = ge_new_gif(
-            "test_rgb.gif", W, H,
+            "mars_night_rot.gif", W, H,
             palette, 8, -1, 1);
 
         // pixels contains the palette index of each x, y of the frame.
         uint8_t *pixels = malloc(W*H*sizeof(uint8_t));
         memset(pixels, 0, W*H*sizeof(uint8_t));
 
-        int n_frames = 20;
+        // int n_frames = 40*6.28;
+        int n_frames = 10;
         for (int f = 0; f < n_frames; f++) {
             printf("%d/%d\n", f, n_frames);
             float t = f / 40.;
 
             make_zs(zs, zs_valid, t);
-            fill_texture_gif(pixels, zs, zs_valid, t);
+            fill_texture(pixels, zs, zs_valid, t, 0);
 
             // Necessary to do for every frame separately
             memcpy(gif->frame, pixels, W*H*sizeof(uint8_t));
